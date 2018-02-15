d5a5981a2fb2135fb2c2ddc6b6dcf948b35d5ad2
Unknown <olivier.serris@wanadoo.fr>
2018-02-15 17:38:36 +0100

    Added Ybot + Weapon empty ActorComponenet

---
+++ Source/HackAndSlashProject/MeleeWeapon.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon.h"
+#include "MeleeWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HACKANDSLASHPROJECT_API UMeleeWeapon : public UWeapon
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
