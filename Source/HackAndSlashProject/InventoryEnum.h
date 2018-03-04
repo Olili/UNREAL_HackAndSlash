4ea755efccde7f2d69395b85a9182bb44d51fb13
Unknown <olivier.serris@wanadoo.fr>
2018-03-04 17:15:44 +0100

    Added empty inventory object + itemType enum

---
+++ Source/HackAndSlashProject/InventoryEnum.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+
+UENUM(BlueprintType, meta = (Bitflags))		//"BlueprintType" is essential to include
+enum class EItemType : uint8
+{
+	Weapon 	UMETA(DisplayName = "Weapon"),
+	Helmet 	UMETA(DisplayName = "Helmet"),
+	Armor 	UMETA(DisplayName = "Armor"),
+	Feet 	UMETA(DisplayName = "Feet"),
+	Arm 	UMETA(DisplayName = "Arm"),
+	Ring	UMETA(DisplayName = "Ring")
+};
+
